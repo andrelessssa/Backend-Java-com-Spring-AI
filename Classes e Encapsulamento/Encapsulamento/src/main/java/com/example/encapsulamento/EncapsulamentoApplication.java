dcbd1eecef3dfca24b9f60054ea637a10292877f
andre.lessa <andrelessa013@gmail.com>
2026-05-21 13:25:29 -0300

    Teste
    Teste

--- Classes e Encapsulamento/Encapsulamento/src/main/java/com/example/encapsulamento/EncapsulamentoApplication.java
+++ Classes e Encapsulamento/Encapsulamento/src/main/java/com/example/encapsulamento/EncapsulamentoApplication.java
@@ -19,9 +19,10 @@ public class EncapsulamentoApplication {
       //  System.out.println("Nome: " + pessoa.getNome());
       //  System.out.println("Idade: " + pessoa.getIdade());
 
-
-
-
+        var pessoaRecord = new PessoaRecord("Joao", 20);
+        System.out.println(pessoaRecord);
+        
+       
     }
 
 }
---
+++ Classes e Encapsulamento/Encapsulamento/src/main/java/com/example/encapsulamento/PessoaRecord.java
@@ -0,0 +1,6 @@
+package com.example.encapsulamento;
+
+public record PessoaRecord(String nome, int idade) {
+
+    
+}
